9c726d7ab7e628ad073144d192e7fdefa5dd0299
femelo22 <luiz123jfmg@gmail.com>
2022-03-14 17:50:31 -0300

    feat: add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,11 +27,13 @@ import { MatCardModule } from '@angular/material/card';
 //componentes do projeto
 import { NavComponent } from './components/nav/nav.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
