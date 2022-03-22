07d3b8b5c0d366d3eda8c2ac36cdf7ce0b2f53b8
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2022-03-22 20:41:55 -0300

    feat: implements AuthInterceptorProvider

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,8 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -12,7 +13,23 @@ export class AuthInterceptor implements HttpInterceptor {
 
   constructor() {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token = localStorage.getItem('token');
+
+    if(token) {
+      const cloneRequisicao = request.clone( {headers: request.headers.set('Authorization', `Bearer ${token}`)} );
+      return next.handle(cloneRequisicao);
+    } else{
+      return next.handle(request);
+    }
   }
+
 }
+
+export const AuthInterceptorProvider = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+]
