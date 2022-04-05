0a5a8542870a01fbbd30c7a75fa8c97ad15748a7
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2022-04-05 08:14:37 -0300

    chamado list

--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+import { Chamado } from '../models/chamado';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChamadoService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  findAll(): Observable<Chamado[]> {
+    return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`)
+  }
+
 }
